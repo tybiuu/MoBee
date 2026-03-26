a8f04487c74d91e08e686391bb178200136b7bd1
tybiuu <tcasafrancacarrillo@gmail.com>
2026-03-26 16:23:07 -0500

    Initial commit

---
+++ MoBee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoBee
+//
+//  Created by Tania on 26/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoBee/MoBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoBeeApp.swift
+//  MoBee
+//
+//  Created by Tania on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MoBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
